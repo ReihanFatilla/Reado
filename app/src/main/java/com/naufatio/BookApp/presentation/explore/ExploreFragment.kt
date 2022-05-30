6d116e03f7d2eb696f3a8d8fd913054878321f4b
ReihanFatilla <reihanfatilla@gmail.com>
2022-05-30 20:11:07 +0700

    (feat): cleaned starter project

---
+++ app/src/main/java/com/naufatio/BookApp/presentation/explore/ExploreFragment.kt
@@ -0,0 +1,32 @@
+package com.naufatio.BookApp.presentation.explore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.naufatio.BookApp.databinding.FragmentExploreBinding
+
+class ExploreFragment : Fragment() {
+
+    private var _binding: FragmentExploreBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentExploreBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/naufatio/BookApp/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/naufatio/BookApp/presentation/home/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.naufatio.BookApp.ui.home
+package com.naufatio.BookApp.presentation.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -13,8 +13,6 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,17 +20,10 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
