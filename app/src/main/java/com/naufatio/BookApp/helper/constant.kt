5b12391d34011593620ad56b57dd2beacac4aea0
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2022-05-31 14:36:47 +0700

    (feat): created list of category for "Books for you" section

--- app/src/main/java/com/naufatio/BookApp/helper/constant.kt
+++ app/src/main/java/com/naufatio/BookApp/helper/constant.kt
@@ -2,4 +2,14 @@ package com.naufatio.BookApp.helper
 
 object constant {
 
+    val BooksRecommendation = arrayListOf<String>(
+        "Technology",
+        "Help",
+        "Business",
+        "Apple",
+        "Stock",
+        "Crypto",
+        "Improvements"
+    )
+
 }
