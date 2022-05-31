f5fc96dbfe70216b1f9ff674e45d914f6ee61905
ReihanFatilla <reihanfatilla@gmail.com>
2022-05-31 10:22:10 +0700

    (feat): changed tab bar adapter

--- app/src/main/java/com/naufatio/BookApp/presentation/home/adapter/BookTabbarAdapter.kt
+++ app/src/main/java/com/naufatio/BookApp/presentation/home/adapter/BookTabbarAdapter.kt
@@ -2,13 +2,21 @@ package com.naufatio.BookApp.presentation.home.adapter
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.naufatio.BookApp.data.BooksResponse
 import com.naufatio.BookApp.databinding.RowItemHomeTabBarBinding
+import com.naufatio.BookApp.helper.OnItemClickCallback
 
 class BookTabbarAdapter:RecyclerView.Adapter<BookTabbarAdapter.MyViewHolder>() {
 
-    private var listBooksTabbar = ArrayList<String>()
+    private var listBooksTabbar = ArrayList<BooksResponse>()
 
-    fun setData(data: List<String>?) {
+    private var onItemClickCallBack: OnItemClickCallback? = null
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallBack = onItemClickCallback
+    }
+
+    fun setData(data: List<BooksResponse>?) {
         if (data == null) return
         listBooksTabbar.clear()
         listBooksTabbar.addAll(data)
@@ -26,6 +34,10 @@ class BookTabbarAdapter:RecyclerView.Adapter<BookTabbarAdapter.MyViewHolder>() {
             tvTitleBook.text;
             tvAuthorBook.text;
             tvRatingBook.text;
+        }
+
+        holder.itemView.setOnClickListener {
+            onItemClickCallBack?.onItemClicked(data)
         }
     }
 
