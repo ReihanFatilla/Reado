3b3196c0ae19cddcd2e3a545be429d9ea2aa7d51
ReihanFatilla <reihanfatilla@gmail.com>
2022-05-31 08:32:27 +0700

    (feat): added depedencies and base url

---
+++ app/src/main/java/com/naufatio/BookApp/data/remote/ApiService.kt
@@ -0,0 +1,13 @@
+package com.naufatio.BookApp.data.remote
+
+import io.reactivex.rxjava3.core.Flowable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("volumes")
+    fun searchBook(
+        @Query("q")
+        query: String
+    )
+}
---
+++ app/src/main/java/com/naufatio/BookApp/helper/constant.kt
@@ -0,0 +1,5 @@
+package com.naufatio.BookApp.helper
+
+object constant {
+
+}
