1d1530d787d2f6a4ea28e1ea0fcd1c8c61f909dd
ReihanFatilla <reihanfatilla@gmail.com>
2022-06-02 08:21:25 +0700

    (feat): added searchview sort by each method

--- app/src/main/java/com/naufatio/BookApp/presentation/explore/ExploreFragment.kt
+++ app/src/main/java/com/naufatio/BookApp/presentation/explore/ExploreFragment.kt
@@ -23,9 +23,44 @@ class ExploreFragment : Fragment() {
 
         _binding = FragmentExploreBinding.inflate(inflater, container, false)
 
-        //remove actionbar
-        activity?.actionBar?.hide()
+        setUpSortByMenu()
+
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.cvSortBy.visibility = View.GONE
+    }
+
+    private fun setUpSortByMenu() {
+        binding.svBook.setOnQueryTextListener(object :
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                binding.apply {
+                    cvSortBy.visibility = View.VISIBLE
+                    tvSearchTitle.text = newText
+                    tvSearchCategory.text = newText
+                    tvSearchPublisher.text = newText
+                    tvSearchAuthor.text = newText
+                }
+                return false
+            }
+
+        })
+
+        binding.svBook.setOnQueryTextFocusChangeListener { _, b ->
+            if (!b) {
+                binding.cvSortBy.visibility = View.GONE
+
+            } else {
+                binding.cvSortBy.visibility = View.VISIBLE
+            }
+        }
     }
 
     override fun onDestroyView() {
